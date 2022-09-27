1e4e98c54687c209880516889a2c1b10a32d4721
Marc Brgs <marc.brgs@yahoo.fr>
2022-09-27 14:03:44 +0200

    Dash system
    - Left click for dash (8 different directions)

--- WindJammersMCTS/Assets/Scripts/PlayerController.cs
+++ WindJammersMCTS/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float moveSpeed = 26f;
+
+    private Rigidbody rb;
+
+    private float horizontalInput = 0f;
+    private float verticalInput = 0f;
+
+    private float dashingTime = 0.2f;
+    private bool isDashing = false;
+    private bool canDash = true;
+    private float dashingPower = 30f;
+    private float dashX = 0f;
+    private float dashY = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInputs();
+    }
+    void FixedUpdate()
+    {
+        if (isDashing) return;
+        Move();
+    }
+
+    private void GetInputs()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButtonDown(1))
+            StartCoroutine(Dash());
+    }
+    private void Move()
+    { 
+        rb.velocity = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed;
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        dashX = 0f;
+        dashY = 0f;
+        if (horizontalInput != 0f)
+            dashX = horizontalInput / Mathf.Abs(horizontalInput);
+        if(verticalInput != 0f)
+            dashY = verticalInput / Mathf.Abs(verticalInput);
         
+        rb.velocity = new Vector3(dashX, 0f, dashY) * dashingPower;
+        
+        
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        canDash = true;
     }
 }
