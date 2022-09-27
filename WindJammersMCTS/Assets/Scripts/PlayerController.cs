01e11e8cf22954e4c43106e868d169b5afe37423
MatthieuL <61740378+Matthieu39@users.noreply.github.com>
2022-09-27 14:10:41 +0100

    Manette

--- WindJammersMCTS/Assets/Scripts/PlayerController.cs
+++ WindJammersMCTS/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput playerInput;
+    private InputAction Shoot;
+
+
+
+    private void Awake()
     {
-        
+        playerInput = new PlayerInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerInput.Player.Shoot.performed += DoShoot;
+        playerInput.Player.Shoot.Enable();
+    }
+
+    private void DoShoot(InputAction.CallbackContext obj)
+    {
+        FrisbeeController.instance.Shoot();
     }
 }
